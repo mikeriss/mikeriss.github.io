b31d318a4d6732bedcde93a5447b81094ad5b649
mikeriss <mypic@mail.com>
2019-06-03 01:14:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function(){
+    debugger;
+    document.getElementById("Title").innerHTML = "Click Event";
+  });
+
+  document.addEventListener('touchstart', function(e){
+    document.getElementById("TouchEvent").innerHTML = "Click Event";
+});
